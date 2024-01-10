60966802113d2e817e8c9dde6fd53bca5a5be33c
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-01-10 15:42:32 +0100

    Add axios

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 // Estraggo la funzione createApp dall'oggetto Vue
 const { createApp } = Vue;
 
+
 // Creo l'istanza di Vue da mondare in pagina
 createApp({
     data() {
         return {
             message: 'Template HTML CSS Vue',
         };
+    },
+    mounted(){
+        console.log(axios);
+
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function (risposta){
+                console.log(risposta);
+                console.log(risposta.data);
+            });
     }
   // Monto l'istanza di Vue in pagina
 }).mount('#app');
